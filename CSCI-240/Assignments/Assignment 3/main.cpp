9778752fc1a49de0ab86a55e79c590ea38d593fa
scott sheble <117122862+scottsheble@users.noreply.github.com>
2023-09-22 19:59:13 -0500

    Format

--- CSCI-240/Assignments/Assignment 3/main.cpp
+++ CSCI-240/Assignments/Assignment 3/main.cpp
@@ -28,41 +28,37 @@ const double BRACKET_LIMIT2 = (44725);
 const double BRACKET_LIMIT3 = (95375);
 const double BRACKET_LIMIT4 = (95375);
 
-//double grossIncome = 0;
-double grossIncome = (75611.34); //test case 1
+double grossIncome = (0); 
 double incomeTax = 0;
 double incomeAboveBracket = 0;
 
 int main()
 {
-    //std::cout << "What is your gross income? ";
-    std::cout << "What is your gross income? (test case 1)"; //test case 1
-    //std::cin >> grossIncome;
-    if (grossIncome <= BRACKET_LIMIT1) 
+    std::cout << "What is your gross income? ";
+    std::cin >> grossIncome;
+    if (grossIncome <= BRACKET_LIMIT1) // check against bracket limit 1
     {
         incomeTax = (TAX_BRACKET1 * grossIncome);
     }
-    else if (grossIncome > BRACKET_LIMIT1 && grossIncome <= BRACKET_LIMIT2) 
+    else if (grossIncome > BRACKET_LIMIT1 && grossIncome <= BRACKET_LIMIT2) // check against bracket limit 2 and bracket limit 1
     {
-        incomeAboveBracket = grossIncome - BRACKET_LIMIT1;
-        incomeTax = (0.1 * 11000) + (0.12 * incomeAboveBracket);
+        incomeAboveBracket = grossIncome - BRACKET_LIMIT1; // finds how much income is taxable above the bracket
+        incomeTax = (0.1 * 11000) + (0.12 * incomeAboveBracket); 
     }
-    else if (grossIncome > BRACKET_LIMIT2 && grossIncome <= BRACKET_LIMIT3)
+    else if (grossIncome > BRACKET_LIMIT2 && grossIncome <= BRACKET_LIMIT3) // check against bracket limit 2 and bracket limit 3
     {
-        incomeAboveBracket = grossIncome - BRACKET_LIMIT2;
+        incomeAboveBracket = grossIncome - BRACKET_LIMIT2; // finds how much income is taxable above the bracket
         incomeTax = (0.1 * 11000) + (0.12 * 33725) + (0.22 * incomeAboveBracket);
-        std::cout << "t3";
     }
-    else if (grossIncome > BRACKET_LIMIT4)
+    else if (grossIncome > BRACKET_LIMIT4) // check against bracket limit 4
     {
-        incomeAboveBracket = grossIncome - BRACKET_LIMIT4;
+        incomeAboveBracket = grossIncome - BRACKET_LIMIT4; // finds how much income is taxable above the bracket
         incomeTax = (0.1 * 11000) + (0.12 * 33725) + (0.22 * 50650) + (0.24 * incomeAboveBracket);
-        std::cout << "t4";
     }
         
     std::cout << std::endl;
-    std::cout << "Gross Income: " << fixed << setprecision(2) <<  grossIncome << std::endl;
-    std::cout << "Income Tax " << fixed << setprecision(2) << incomeTax << std::endl;
+    std::cout << "Gross Income:          $ " << fixed << setprecision(2) << right << setw(5) << fixed << grossIncome << std::endl;
+    std::cout << "Income Tax:            $ " << fixed << setprecision(2) << right << setw(5) << fixed << incomeTax << std::endl;
     std::cout << std::endl;
     return 0;
 }
