d318c85b5f328bd4d82a7b932d41420b436a77b9
scott sheble <117122862+scottsheble@users.noreply.github.com>
2023-09-22 22:37:07 -0500

    Doxygen documentation

--- CSCI-340/LocalClones/02b-simple-list-scottsheble-LocalClone/simple_linked_list.h
+++ CSCI-340/LocalClones/02b-simple-list-scottsheble-LocalClone/simple_linked_list.h
@@ -18,7 +18,16 @@
 
 //#warning The provided version of simple_linked_list.h does not actually implement things. 
 
-
+/**
+ * @brief Construct a new simple linked list<T>::simple linked list object setting 
+ * "head" to "nullptr", "tail" to "nullptr", and setting the number of elements
+ * (n_elements) in the linked list to 0.
+ * 
+ * @tparam T 
+ * @tparam ITERATOR 
+ * @param beg 
+ * @param end 
+ */
 template <typename T>
 template <typename ITERATOR> //constructor copying the contents of another container
 simple_linked_list<T>::simple_linked_list(ITERATOR beg, ITERATOR end) : n_elements(0), head(nullptr), tail(nullptr) {
@@ -26,7 +35,14 @@ simple_linked_list<T>::simple_linked_list(ITERATOR beg, ITERATOR end) : n_elemen
         push_back(*it);
     }
 }
-
+/**
+ * @brief Boolean returning true if the linked list is empty and false
+ *  if it does contain elements.
+ * 
+ * @tparam T 
+ * @return true 
+ * @return false 
+ */
 template <typename T>
 bool simple_linked_list<T>::empty() const {
   if(head == nullptr || n_elements == 0){
@@ -36,17 +52,35 @@ bool simple_linked_list<T>::empty() const {
   }
 }
 
-
+/**
+ * @brief This function returns the beginning of the linked list.
+ * 
+ * @tparam T 
+ * @return simple_linked_iterator<T> 
+ */
 template <typename T>
 simple_linked_iterator<T> simple_linked_list<T>::begin() {
     return simple_linked_iterator<T>(head);
 }
 
+/**
+ * @brief This function returns the end of the linked list.
+ * 
+ * @tparam T 
+ * @return simple_linked_iterator<T> 
+ */
 template <typename T>
 simple_linked_iterator<T> simple_linked_list<T>::end() {
     return simple_linked_iterator<T>(nullptr);
 }
 
+/**
+ * @brief This function returns a reference to the data member of the head node.
+ * Throws an out_of_range exception if the container is empty.
+ * 
+ * @tparam T 
+ * @return T& 
+ */
 template <typename T>
 T & simple_linked_list<T>::front() {
     if (empty()) {
@@ -54,7 +88,14 @@ T & simple_linked_list<T>::front() {
     }
     return head->data;
 }
-    
+
+/**
+ * @brief This function returns a reference to the data member of the tail node.
+ * Throws an out_of_range exception if the container is empty.
+ * 
+ * @param T
+ * @return T& 
+ */
 template <typename T>
 T & simple_linked_list<T>::back() {
     if (empty()) {
@@ -63,6 +104,12 @@ T & simple_linked_list<T>::back() {
     return tail->data;
 }
 
+/**
+ * @brief Modifies the container to remove the last node and thows an out_of_range
+ * exception if the container is empty.
+ * 
+ * @tparam T 
+ */
 template <typename T>
 void simple_linked_list<T>::pop_back() {
     if (empty()) {
@@ -83,6 +130,12 @@ void simple_linked_list<T>::pop_back() {
     n_elements--;
 }
 
+/**
+ * @brief Adds a new node containing the provided value to the end of the container.
+ * 
+ * @tparam T 
+ * @param x 
+ */
 template <typename T>
 void simple_linked_list<T>::push_back(const T &x) {
     linked_node<T> *new_node = new linked_node<T>(x);
@@ -95,16 +148,34 @@ void simple_linked_list<T>::push_back(const T &x) {
     n_elements++;
 }
 
+/**
+ * @brief Called when the container goes out of scope and deletes any new objects
+ * created.
+ * 
+ * @tparam T 
+ */
 template <typename T>
 simple_linked_list<T>::~simple_linked_list() {
     clear();
 }
 
+/**
+ * @brief Returns how many elements are currently in the container.
+ * 
+ * @tparam T 
+ * @return size_t 
+ */
 template <class T>
 size_t simple_linked_list<T>::size() const {
     return n_elements;
 }
 
+/**
+ * @brief Removes all of the nodes from the container and resets it to an empty
+ * state.
+ * 
+ * @tparam T 
+ */
 template <typename T>
 void simple_linked_list<T>::clear() {
     while (!empty()) {
@@ -114,28 +185,57 @@ void simple_linked_list<T>::clear() {
 
 // IMPLEMENTATIONS FOR ITERATOR
 
+/**
+ * @brief This returns "true" if and only if both simple_linked_iterators 
+ * (*this and the parameter) are pointing to the same node.
+ * 
+ * @tparam T 
+ * @param other 
+ * @return true 
+ * @return false 
+ */
 template <typename T>
 bool simple_linked_iterator<T>::operator == (const simple_linked_iterator<T> &other) const {
     return pos == other.pos;
 }
 
+/**
+ * @brief Called when you increment your iterator with ++ before the name, i.e. ++i. 
+ * This changes the current iterator so it points to the next node, and returns a 
+ * reference to the current iterator, which will be at the new position.
+ * 
+ * @tparam T 
+ * @return simple_linked_iterator <T>& 
+ */
 template <typename T>
-
-//preincrement
 simple_linked_iterator <T> & simple_linked_iterator <T>::operator ++ () { 
     pos = pos->next;
 
     return *this;
 }
   
-//postincrement
+/**
+ * @brief Called when you increment your iterator with ++ after the variable name, i.e. i++. 
+ * This also changes the current iterator so it points to the next node, but it returns 
+ * another iterator that points to the original node position.
+ * 
+ * @tparam T 
+ * @return simple_linked_iterator <T> 
+ */
 template <typename T>
 simple_linked_iterator <T> simple_linked_iterator<T>::operator ++ (int) { 
     simple_linked_iterator <T> temp = *this;
     ++(*this);
     return temp;
 }
-    
+
+/**
+ * @brief This should return a reference to the data member of the linked_node that the 
+ * current iterator points to. 
+ * 
+ * @tparam T 
+ * @return T& 
+ */
 template <typename T>
 T & simple_linked_iterator<T>::operator * () { // dereference operator
     if (!pos) {
