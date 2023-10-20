4dc5ee53e49177e57da346247542ddbf1fa4341f
scott sheble <117122862+scottsheble@users.noreply.github.com>
2023-10-20 17:59:14 -0500

    changes

---
+++ CSCI-240/LocalAssignments/Assignment-6/main.cpp
@@ -0,0 +1,23 @@
+/********************************************************
+    CSCI 240        Program 3       Fall 2023
+
+    Programmer: Scott Sheble
+
+    Section: 1
+
+    Date Due: 10/20/23
+
+    Purpose: For this assignment, the focus will be on 
+    writing and calling functions. The functions will be 
+    used to write a program that completes a series of 
+    computation exercises. A menu of different options 
+    will be shown to a user at the beginning of the 
+    program (and again after each function's objective 
+    has been concluded). The user will select an option 
+    and the program will then prompt for additional 
+    information that is specific to that option. 
+    This process will continue until the user decides 
+    to quit.
+
+    Created by scott sheble on 10/01/23.
+*********************************************************/
