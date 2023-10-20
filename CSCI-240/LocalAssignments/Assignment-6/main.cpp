4b8ed11bd358c98fce96098f4b7f937a27ef3630
scott sheble <117122862+scottsheble@users.noreply.github.com>
2023-10-20 18:19:33 -0500

    definitions

--- CSCI-240/LocalAssignments/Assignment-6/main.cpp
+++ CSCI-240/LocalAssignments/Assignment-6/main.cpp
@@ -21,3 +21,64 @@
 
     Created by scott sheble on 10/01/23.
 *********************************************************/
+using namespace std;
+
+int Menu(); //This function displays a menu and gets a choice from the user.
+
+int getValue( string prompt, int lowerBound, int upperBound ); //This function will get an integer value from the user that is within a specified range.
+
+int sumOfSquares( int limit ); // This function will summate all the squares of positive integers between 1 and limit (inclusive), and return that sum.
+
+double averageOfCubes( int limit ); //This function will calculate the average of all the cubes of 'limit' number of integers, starting with 0, and return that value.
+
+int sumOfASCII( int limit ); //This function will calculate the sum of the ASCII values of the first N characters starting with 'a' or 'A', and return that sum.
+
+void NIUchant(int iterations); //This function will display an iterations number of words from the NIU school chant: Forward! Together, Forward! Make sure to include spaces after each word or token, as well as two (2) newline characters once the list of words has finished printing.
+
+int Menu()
+{
+    return 0;
+}
+
+int getValue(string prompt, int lowerBound, int upperBound)
+{
+    return 0;
+}
+
+int sumOfSquares(int limit)
+{
+    return 0;
+}
+
+double averageOfCubes(int limit)
+{
+    return 0.0;
+}
+
+int sumOfASCII(int limit)
+{
+    return 0;
+}
+
+void NIUchant(int iterations)
+{
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
