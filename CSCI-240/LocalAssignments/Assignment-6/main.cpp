10646fbfa4e950592278bc2e237bd041cc6c0488
scott sheble <117122862+scottsheble@users.noreply.github.com>
2023-10-20 21:44:56 -0500

    yes]

--- CSCI-240/LocalAssignments/Assignment-6/main.cpp
+++ CSCI-240/LocalAssignments/Assignment-6/main.cpp
@@ -44,7 +44,8 @@ return 0;
 
 
 /**
- * @brief This function displays a menu and gets a choice from the user.
+ * @brief This function displays a menu and gets a choice from 
+ * the user.
  * 
  * @return int 
  */
@@ -59,12 +60,14 @@ int Menu()
     std::cout << "Enter your choice:" << std::endl;
 
 
-    
+
     return 0;
 }
 
 /**
- * @brief Get the Value object. This function will get an integer value from the user that is within a specified range.
+ * @brief Get the Value object. This function will get an 
+ * integer value from the user that is within a specified 
+ * range.
  * 
  * @param prompt 
  * @param lowerBound 
@@ -77,7 +80,9 @@ int getValue(string prompt, int lowerBound, int upperBound)
 }
 
 /**
- * @brief This function will summate all the squares of positive integers between 1 and limit (inclusive), and return that sum.
+ * @brief This function will summate all the squares of 
+ * positive integers between 1 and limit (inclusive), and 
+ * return that sum.
  * 
  * @param limit 
  * @return int 
@@ -88,7 +93,9 @@ int sumOfSquares(int limit)
 }
 
 /**
- * @brief This function will calculate the average of all the cubes of 'limit' number of integers, starting with 0, and return that value.
+ * @brief This function will calculate the average of all the 
+ * cubes of 'limit' number of integers, starting with 0, and 
+ * return that value.
  * 
  * @param limit 
  * @return double 
@@ -99,7 +106,9 @@ double averageOfCubes(int limit)
 }
 
 /**
- * @brief This function will calculate the sum of the ASCII values of the first N characters starting with 'a' or 'A', and return that sum.
+ * @brief This function will calculate the sum of the ASCII 
+ * values of the first N characters starting with 'a' or 'A', 
+ * and return that sum.
  * 
  * @param limit 
  * @return int 
@@ -110,7 +119,11 @@ int sumOfASCII(int limit)
 }
 
 /**
- * @brief This function will display an iterations number of words from the NIU school chant: Forward! Together, Forward! Make sure to include spaces after each word or token, as well as two (2) newline characters once the list of words has finished printing.
+ * @brief This function will display an iterations number of 
+ * words from the NIU school chant: Forward! Together, 
+ * Forward! Make sure to include spaces after each word or 
+ * token, as well as two (2) newline characters once the list 
+ * of words has finished printing.
  * 
  * @param iterations 
  */
